8f2206dff6221c535e1e8ad56e336621286c078e
saebyeok2 <tomato8269@naver.com>
2023-10-03 11:06:49 +0900

    HTTP Servlet Response 실습

---
+++ src/main/java/project/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,30 @@
+package project.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import project.servlet.basic.HelloData;
+
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type: application/json
+        response.setHeader("content-type", "application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData data = new HelloData();
+        data.setUsername("kim");
+        data.setAge(20);
+        //{"username":"kim","age":20}
+        String result = objectMapper.writeValueAsString(data);
+        response.getWriter().write(result);
+    }
+}
