0e9b866bb69d8cd9878957a00b441f5c5cf70680
saebyeok2 <tomato8269@naver.com>
2023-10-06 15:13:16 +0900

    MVC 프레임워크 - 프론트 컨트롤러 도입

---
+++ src/main/java/project/servlet/web/frontcontroler/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,30 @@
+package project.servlet.web.frontcontroler.v1.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import project.servlet.domain.member.Member;
+import project.servlet.domain.member.MemberRepository;
+import project.servlet.web.frontcontroler.v1.ControllerV1;
+
+import java.io.IOException;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
